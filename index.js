b0ba6847db6988f62d18a0425839e16839d6bf43
Benadette Mokandu <benadettemokandu@gmail.com>
2025-06-29 11:19:31 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Add your code here
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  })
+    .then(response => response.json())
+    .then(data => {
+      const newElement = document.createElement("p");
+      newElement.textContent = data.id;
+      document.body.appendChild(newElement);
+    })
+    .catch(error => {
+      const errorElement = document.createElement("p");
+      errorElement.textContent = error.message;
+      document.body.appendChild(errorElement);
+    });
+}
